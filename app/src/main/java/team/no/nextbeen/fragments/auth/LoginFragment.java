8a30cdd7803270bfc628f72e7cca6ab0933f7f9f
hoaitran308 <tranvanhoai.tt4@gmail.com>
2022-04-14 01:27:39 +0700

    implement firebase to handle login

--- app/src/main/java/team/no/nextbeen/fragments/auth/LoginFragment.java
+++ app/src/main/java/team/no/nextbeen/fragments/auth/LoginFragment.java
@@ -1,5 +1,6 @@
 package team.no.nextbeen.fragments.auth;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -12,13 +13,17 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+import team.no.nextbeen.MainActivity;
 import team.no.nextbeen.R;
 
 public class LoginFragment extends Fragment {
 
+    private FirebaseAuth mAuth;
     private EditText editTextEmail, editTextPassword;
-    private Button buttonLogin;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -36,11 +41,42 @@ public class LoginFragment extends Fragment {
     public void onStart() {
         super.onStart();
 
+        mAuth = FirebaseAuth.getInstance();
+
         editTextEmail = requireView().findViewById(R.id.editTextEmail);
         editTextPassword = requireView().findViewById(R.id.editTextPassword);
-        buttonLogin = requireView().findViewById(R.id.buttonLogin);
+
+        Button buttonLogin = requireView().findViewById(R.id.buttonLogin);
+        buttonLogin.setOnClickListener(view -> processLogin());
+
         TextView textViewRegister = requireView().findViewById(R.id.textViewRegister);
         textViewRegister.setOnClickListener(view -> replaceRegisterFragment());
+    }
+
+    private void processLogin() {
+        String emailAddress = editTextEmail.getText().toString();
+        String password = editTextPassword.getText().toString();
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(emailAddress).matches()) {
+            editTextEmail.setError(getString(R.string.auth_login_error_invalid_email));
+            editTextEmail.requestFocus();
+        }
+        else if (password.length() < 6) {
+            editTextPassword.setError(getString(R.string.auth_login_error_invalid_password));
+            editTextPassword.requestFocus();
+        }
+        else {
+            mAuth.signInWithEmailAndPassword(editTextEmail.getText().toString(), editTextPassword.getText().toString())
+                    .addOnCompleteListener(requireActivity(), task -> {
+                        if (task.isSuccessful()) {
+                            showNotification(getString(R.string.auth_login_notification_success));
+                            Intent intent = new Intent(requireContext(), MainActivity.class);
+                            startActivity(intent);
+                        }
+                        else {
+                            showNotification(getString(R.string.auth_login_notification_fail));
+                        }
+                    });
+        }
     }
 
     private void replaceRegisterFragment() {
@@ -48,5 +84,9 @@ public class LoginFragment extends Fragment {
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frame_layout_auth, new RegisterFragment());
         fragmentTransaction.commit();
+    }
+
+    private void showNotification(String content) {
+        Toast.makeText(requireContext(), content, Toast.LENGTH_LONG).show();
     }
 }
